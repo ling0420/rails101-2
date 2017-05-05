5a1fb6edbb472360076a8032a3042e95a2850089
Leo <17959547@qq.com>
2017-05-05 12:20:13 +0800

    join @ quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -34,6 +34,7 @@ class GroupsController < ApplicationController
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
+
   def update
 
     if @group.update(group_params)
@@ -51,6 +52,30 @@ class GroupsController < ApplicationController
 
     redirect_to groups_path
 
+  end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:warning] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你已经是本讨论版成员了"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "你不是本讨论版成员，怎么退出 xd"
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
